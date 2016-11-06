1f652cd37b031fec860f1ac05a2c47287dc368c7
robinlyang <robiny@gmail.com>
2016-11-05 22:11:23 -0600

    Initial Project

--- SINApp/Program.cs
+++ SINApp/Program.cs
@@ -10,6 +10,48 @@ namespace SINApp
     {
         static void Main(string[] args)
         {
+            string sinNumberStr = "";
+            Console.WriteLine("Please enter your SIN:");
+            sinNumberStr = Console.ReadLine();
+            char[] sinNumberChar = sinNumberStr.ToCharArray();
+            int[] sinNumberInt = new int[sinNumberChar.Length];
+            for (int x = 0; x < sinNumberChar.Length; x++)
+            {
+                sinNumberInt[x] = Convert.ToInt32(sinNumberChar[x].ToString());
+            }
+            int total = 0;
+
+            for (int x = 0; x < sinNumberInt.Length; x++)
+            {
+                int num1 = 0;
+                int num2 = 0;
+                if(x % 2==0)
+                {
+                    num1 = sinNumberInt[x] * 1;
+                    if (num1 > 9)
+                    {
+                        num2 = (num1 / 10) + (num1 % 10);
+                        total = total + num2;
+                    }
+                    else
+                        total = total + num1;
+                }
+                else
+                {
+                    num1 = sinNumberInt[x] * 2;
+                    if (num1 > 9)
+                    {
+                        num2 = (num1 / 10) + (num1 % 10);
+                        total = total + num2;
+                    }
+                    else
+                        total = total + num1;
+                }
+            }
+
+            Console.WriteLine("Total: " + total);
+            
+            Console.ReadLine();
         }
     }
 }
